34ee2118ad4fc9271ddf42180034328440972c85
mirry18299 <141658211+mirry18299@users.noreply.github.com>
2023-09-16 08:23:40 +0300

    products-landing page

---
+++ script.js
@@ -0,0 +1,18 @@
+let mobile_menu_icon = document.querySelector(".mobile_menu_icon");
+let mobile_menu_icon_top = document.querySelector(".mobile_menu_icon_top");
+let mobile_menu_icon_middle = document.querySelector(".mobile_menu_icon_middle");
+let mobile_menu_icon_bottom = document.querySelector(".mobile_menu_icon_bottom");
+let ul = document.querySelector("ul");
+let menu_status = 0;
+
+mobile_menu_icon.addEventListener("click", ()=>{
+    if(menu_status === 0){
+        ul.style.top = "0";
+        mobile_menu_icon_top.classList.add("mobile_menu_icon_top_twist");
+        mobile_menu_icon_middle.classList.add("mobile_menu_icon_top_twist");
+        mobile_menu_icon_bottom.classList.add("mobile_menu_icon_top_twist");
+        menu_status = 1;
+    }else{
+        ul.style.top = "-700em"
+    }
+})
